7afcd74a923762f6a4feaf40cfbd4e7028e865e9
Jonathan-Mandl <jonathan.mandl2@gmail.com>
2023-01-24 23:17:15 +0200

    commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/tar4/Ex4
+# Build directory: /root/tar4/Ex4/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
