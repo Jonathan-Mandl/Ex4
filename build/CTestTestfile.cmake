3d95754706eca4c3cbe67edef3f4ed1fbe07ce45
Jonathan-Mandl <118123854+Jonathan-Mandl@users.noreply.github.com>
2023-01-24 23:12:50 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/tar4/Ex4
-# Build directory: /root/tar4/Ex4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
